2ed8e7f14c6a2f895c006afd2f4716fb2c7452d9
Kenny Li <kenny.wj.li@gmail.com>
2021-04-28 19:46:24 -0400

    finishing styling for cart

--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default class Cart extends React.Component{
 
     render(){
+        debugger
         const signin = () => (
             <div>
                 <div className="cart-cont">
@@ -28,7 +29,34 @@ export default class Cart extends React.Component{
         const cart = () => (
             <div>
                 <div className='signin-cart-cont'>
-                    <h1>Shopping Cart</h1>
+                    <h1 >Shopping Cart</h1>
+                    <div>{this.props.cartItems.map((cartItem,idx) => (
+                        <div key={idx} className="cart-product">
+                            <Link to={`/product/${cartItem.product_id}`}><img className='cart-image' src={cartItem.image_url} /></Link>
+                            <div className="cart-title-cont">
+                                <Link to={`/product/${cartItem.product_id}`} className="cart-title">{cartItem.title}</Link>
+                                <p>In Stock</p>
+                                <span className="cart-shipped">Shipped from: <Link to="/">Amazone</Link></span>
+                                <span className="cart-gift">Gift options not available.</span>
+                                <div>
+                                    <select className="cart-select">
+                                        <option value="1">Qty: 1</option>
+                                        <option value="2">Qty: 2</option>
+                                        <option value="3">Qty: 3</option>
+                                        <option value="4">Qty: 4</option>
+                                        <option value="5">Qty: 5</option>
+                                        <option value="6">Qty: 6</option>
+                                        <option value="7">Qty: 7</option>
+                                        <option value="8">Qty: 8</option>
+                                        <option value="9">Qty: 9</option>
+                                    </select>
+                                    <button>Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                    </div>
                 </div>
             </div>
         )

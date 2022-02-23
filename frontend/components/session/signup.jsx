a2337b165fa31e0b842042dfa1c2ea27e908ebf4
Kenny <kenny.wj.li@gmail.com>
2022-02-22 19:35:51 -0500

    fixed up some spacing

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -17,11 +17,9 @@ export default class Signup extends React.Component {
     componentDidMount() {
         this.props.reset()
     }
-
     handleInput(key) {
         return e => this.setState({ [key]: e.target.value })
     }
-
     handleSubmit(e){
         e.preventDefault()
         this.props.signup(this.state)
@@ -45,13 +43,10 @@ export default class Signup extends React.Component {
             return (
                 <ul>
                     {this.props.errors.map((error, idx) => (
-                        
                         <div key={idx} className='errors'>
                             <img id='ex' src={window.exURL}></img>
                             <p className="innerError" id='problem'>There was a problem</p>
-                            <p className="innerError" >
-                                {error}
-                            </p>
+                            <p className="innerError" >{error}</p>
                         </div>
                     ))}
                 </ul>
@@ -61,50 +56,51 @@ export default class Signup extends React.Component {
             return(
                 <ul>
                     <div className="errors long-errors">
-                    <img id='ex' src={window.exURL}></img>
-                    <p className="innerError long-innerError" id='problem'>There was a problem</p>
-                    <p className="innerError long-innerError" >
-                    {this.props.errors.map((error, idx) => {
-                            return <div>- {error}</div>
-                    })}
+                        <img id='ex' src={window.exURL}></img>
+                        <p className="innerError long-innerError" id='problem'>There was a problem</p>
+                        <p className="innerError long-innerError" >
+                            {this.props.errors.map((error, idx) => {
+                                return <div>- {error}</div>
+                            })}
                         </p>
-                                
                     </div>
-                    
                 </ul>
             )
         }
     }
 
     render () {
-        
         return (
             <div className='outer-cont'> 
-            <a href=""><img src={window.brentURL} className="logo" onClick={this.handleLogo}></img></a>
-            <div>{this.renderErrors()}</div>
-            <div className="session-form">
-                <h2 className="createacc">Create account</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="signup-cont">
-                    <label className="signup-param">Your name
-                    <br/>
-                        <input className="signupinput" type="text" value={this.state.name} onChange={this.handleInput('name')} />
-                    </label>
-                        <label className="signup-param">Email
-                        <br />
-                            <input className="signupinput" type="email" value={this.state.email} onChange={this.handleInput('email')}/>
-                    </label >
-                        <label className="signup-param">Password
-                        <br />
-                            <input className="signupinput" type="password" value={this.state.password} placeholder=" At least 6 characters" onChange={this.handleInput('password')}/>
-                            <div id ='clause'><p id="i"> i </p>  Passwords must be at least 6 characters. </div>
-                    </label>
-                    </div>
-                    <button className="signupbutton">Create your Amazone account</button>
-                </form>
+                <a href=""><img src={window.brentURL} className="logo" onClick={this.handleLogo}></img></a>
+                <div>{this.renderErrors()}</div>
+                <div className="session-form">
+                    <h2 className="createacc">Create account</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="signup-cont">
+                            <label className="signup-param">Your name
+                                <br/>
+                                <input className="signupinput" type="text" value={this.state.name} onChange={this.handleInput('name')} />
+                            </label>
+                            <label className="signup-param">Email
+                                <br/>
+                                <input className="signupinput" type="email" value={this.state.email} onChange={this.handleInput('email')}/>
+                            </label>
+                            <label className="signup-param">Password
+                                <br/>
+                                <input className="signupinput" type="password" value={this.state.password} placeholder=" At least 6 characters" onChange={this.handleInput('password')}/>
+                                <div id ='clause'><p id="i"> i </p>  Passwords must be at least 6 characters. </div>
+                            </label>
+                        </div>
+                        <button className="signupbutton">Create your Amazone account</button>
+                    </form>
                     <p className="cond">By creating an account, you agree to Amazone's<br /><li id='break'> <a target="_blank" href="https://www.linkedin.com/in/kenny-li-793b6b15b/" className="form-hyperlink">Conditions of Use</a> and <a className="form-hyperlink" target="_blank" href="https://www.linkedin.com/in/kenny-li-793b6b15b/">Privacy Notice.</a></li></p>
-                    <div id="cont"><p className="already">Already have an account? <a href="" onClick={this.handleRedirect} className="form-hyperlink">Sign-In <GoTriangleRight className="triangle-icon"/></a></p></div>
-            </div>
+                    <div id="cont">
+                        <p className="already">Already have an account? 
+                            <a href="" onClick={this.handleRedirect} className="form-hyperlink">Sign-In <GoTriangleRight className="triangle-icon"/></a>
+                        </p>
+                    </div>
+                </div>
             </div>
         )
     }

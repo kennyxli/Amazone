e267773b9d04691e0772d49255e0046b73d29be3
Kenny <kenny.wj.li@gmail.com>
2022-02-24 00:33:45 -0500

    removed some unused code

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import Root from './components/root'
 import configureStore from './store/store'
 
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     let preloadedState = undefined;
